44d11e6665af5223ef8169970db84a51613913fa
kyontan <kyontan@monora.me>
2021-12-09 03:44:25 +0900

    fix

--- .github/actions/fast-forward-merge/approve-pr-by-bot.js
+++ .github/actions/fast-forward-merge/approve-pr-by-bot.js
@@ -1,6 +1,6 @@
 // called from https://github.com/actions/github-script
 module.exports = async ({ github, context }) => {
-  return octokit.pulls.createReview({
+  return github.rest.pulls.createReview({
     ...context.repo,
     pull_number: context.issue.number,
     event: "APPROVE"
